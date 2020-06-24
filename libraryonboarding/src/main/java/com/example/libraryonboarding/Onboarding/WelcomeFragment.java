ed72980895dc0946c90c6466797e10560d829e01
Ricardo La Madrid <55472623+RicardoLaMadrid@users.noreply.github.com>
2020-06-23 22:27:41 -0400

    initialize commit

---
+++ libraryonboarding/src/main/java/com/example/libraryonboarding/Onboarding/WelcomeFragment.java
@@ -0,0 +1,68 @@
+package com.example.libraryonboarding.Onboarding;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.libraryonboarding.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WelcomeFragment extends Fragment
+{
+
+    private TextView textNext;
+    private ViewPager viewPager;
+    private View view;
+
+    public WelcomeFragment()
+    {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        //initialize viewpager from main activity
+        initializeInflater(inflater, container);
+
+        //initialize components
+        initializeComponents();
+
+        //add click event to text Next
+        eventClickNext();
+
+        //return
+        return view;
+    }
+
+    public void initializeInflater(LayoutInflater inflater, ViewGroup container)
+    {
+        view = inflater.inflate(R.layout.fragment_welcom2, container, false);
+    }
+
+    public void initializeComponents()
+    {
+        viewPager = getActivity().findViewById(R.id.viewPagerContainer);
+        textNext = view.findViewById(R.id.tvNextWelcome);
+    }
+
+    public void eventClickNext()
+    {
+        textNext.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                viewPager.setCurrentItem(1);
+            }
+        });
+    }
+}
